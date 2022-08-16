b5d84e0c963c74257ce17044f570ec7fca5904d7
guidl <guidl@users.noreply.github.com>
2022-08-16 13:16:17 +0000

    map display, code organization

--- JavaSnippets.java
+++ JavaSnippets.java
@@ -1,7 +1,43 @@
 import java.util.*;
+import java.util.stream.IntStream;
 import java.io.*;
 
 class Solution {
-    public static void main(String args[]) {
-              System.out.println("tree_path");
+
+  /* declarations */
+  static Map<Character, State> myMap = new HashMap<>();
+  
+  static int myArray[] = { 1, 2, 3, 4, 5 };
+
+  public static void main(String[] args) {
+
+    System.out.println("tree_path");
+    /* MAP and entry formatting */
+    // split entry with space separator
+    Scanner in = new Scanner(System.in);
+    String input = in.nextLine();
+    for (String state : in.nextLine().split("\\s+")) {
+      // put in a MAP TODO explain more
+      myMap.computeIfAbsent(state.charAt(0), k -> new State(k));
     }
+
+    // display a map
+    System.err.println(myMap.toString());
+    myMap.forEach((k, v) -> System.err.println(" key : " + k + " value : " + v));
+
+    // sort, operations on collections
+    IntStream.of(14, 35, -7, 46, 98).min();
+    Arrays.asList(14, 35, -7, 46, 98).stream().min(Integer::compare);
+    Arrays.asList(14, 35, -7, 46, 98).stream().reduce(Integer::min);
+    Collections.min(Arrays.asList(14, 35, -7, 46, 98));
+
+  }
+}
+
+class State {
+  Character str;
+
+  public State(Character k) {
+    str = k;
+  }
+}
