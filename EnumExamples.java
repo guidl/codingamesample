0e0eab9ca0460107f8077f70dff51a6e42ee6ad6
guidl <guidl@users.noreply.github.com>
2022-08-19 21:41:37 +0200

    Add files via upload
    Start

---
+++ EnumExamples.java
@@ -0,0 +1,43 @@
+enum Direction {
+    N(0, -1),
+    S(0, 1),
+    E(1, 0),
+    W(-1, 0);
+
+    final int diffX;
+    final int diffY;
+
+    Direction(final int diffX, final int diffY) {
+        this.diffX = diffX;
+        this.diffY = diffY;
+    }
+}
+
+public enum Blah {
+    A("text1"),
+    B("text2"),
+    C("text3"),
+    D("text4");
+
+    private String text;
+
+    Blah(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+
+plutôt le value of? oui senseible à la casse 
+    public static Blah fromString(String text) {
+        for (Blah b : Blah.values()) {
+            if (b.text.equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        //return null;
+throw new IllegalArgumentException(v.toString());
+    }
+}
